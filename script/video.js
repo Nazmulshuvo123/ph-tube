710aad743a4f99b1bd4ed44586a91244ce26ae21
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-10-03 15:12:06 +0600

    load categories button form an api and display it

--- script/video.js
+++ script/video.js
@@ -13,8 +13,23 @@ const loadCategories = () => {
 
 //Create loadCategories function
 
-const displayCategories = (data) => {
-    console.log(data);
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById('categories');
+    categories.forEach((elements) => {
+        console.log(elements)
+        //create a button for each categories
+        const button = document.createElement('button');
+        button.classList = 'btn';
+        button.innerText = elements.category;
+
+        //add button categories
+        categoryContainer.append(button);
+    });
+    
 }
 
 
